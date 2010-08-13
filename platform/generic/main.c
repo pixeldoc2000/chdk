c5f5317c9d0bab7d27ca47ee409b711696ef7c5f
pixeldoc <pixeldoc2000@googlemail.com>
2010-08-13 03:41:49 +0200

    platform/generic/main.c cleaned and added some comment

--- platform/generic/main.c
+++ platform/generic/main.c
@@ -10,7 +10,6 @@ extern long link_bss_start;
 extern long link_bss_end;
 extern void boot();
 
-
 static int stop_hooking;
 
 static void (*task_prev)(
@@ -21,13 +20,10 @@ static void (*init_file_modules_prev)(
     long p0,    long p1,    long p2,    long p3,    long p4,
     long p5,    long p6,    long p7,    long p8,    long p9);
 
-
-
 void spytask(long ua, long ub, long uc, long ud, long ue, long uf)
 {
     core_spytask();
 }
-
 
 static void task_start_hook(
     long p0,    long p1,    long p2,    long p3,    long p4,
@@ -38,8 +34,6 @@ static void task_start_hook(
     task_prev(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9 );
 }
 
-
-
 static void init_file_modules_hook(
     long p0,    long p1,    long p2,    long p3,    long p4,
     long p5,    long p6,    long p7,    long p8,    long p9)
@@ -48,14 +42,12 @@ static void init_file_modules_hook(
     init_file_modules_prev(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9 );
 }
 
-
 static void capt_seq_hook(
     long p0,    long p1,    long p2,    long p3,    long p4,
     long p5,    long p6,    long p7,    long p8,    long p9)
 {
     capt_seq_task();
 }
-
 
 static void physw_hook(
     long p0,    long p1,    long p2,    long p3,    long p4,
@@ -71,14 +63,12 @@ static void movie_record_hook(
     movie_record_task();
 }
 
-
-
 static int my_ncmp(const char *s1, const char *s2, long len)
 {
     int i;
-    for (i=0;i<len;i++){
-	if (s1[i] != s2[i])
-	    return 1;
+    for (i=0;i<len;i++) {
+        if (s1[i] != s2[i])
+            return 1;
     }
     return 0;
 }
@@ -90,42 +80,42 @@ void createHook (void *pNewTcb)
 
     // always hook first task creation
     // to create SpyProc
-    if (!stop_hooking){
-	task_prev = (void*)(*entry);
-	*entry = (long)task_start_hook;
-	stop_hooking = 1;
+    if (!stop_hooking) {
+        task_prev = (void*)(*entry);
+        *entry = (long)task_start_hook;
+        stop_hooking = 1;
     } else {
-	// hook/replace another tasks
-	if (my_ncmp(name, "tPhySw", 6) == 0){
-	    *entry = (long)physw_hook;
-	}
+        // hook/replace another tasks
+        if (my_ncmp(name, "tPhySw", 6) == 0) {
+            *entry = (long)physw_hook;
+        }
 
-	if (my_ncmp(name, "tInitFileM", 10) == 0){
-	    init_file_modules_prev = (void*)(*entry);
-	  #if CAM_MULTIPART
-	    *entry = (long)init_file_modules_task;
-	  #else
-	    *entry = (long)init_file_modules_hook;
-	  #endif
-	}
+        if (my_ncmp(name, "tInitFileM", 10) == 0) {
+            init_file_modules_prev = (void*)(*entry);
+            #if CAM_MULTIPART
+                *entry = (long)init_file_modules_task;
+            #else
+                *entry = (long)init_file_modules_hook;
+            #endif
+        }
 
-	if (my_ncmp(name, "tCaptSeqTa", 10) == 0){
-	    *entry = (long)capt_seq_hook;
-	}
+        if (my_ncmp(name, "tCaptSeqTa", 10) == 0) {
+            *entry = (long)capt_seq_hook;
+        }
 
-	if (my_ncmp(name, "tMovieReco", 10) == 0){
-	  #if CAM_CHDK_HAS_EXT_VIDEO_MENU
-	    *entry = (long)movie_record_hook;
-	  #endif
-	}
+        if (my_ncmp(name, "tMovieReco", 10) == 0) {
+            #if CAM_CHDK_HAS_EXT_VIDEO_MENU
+               *entry = (long)movie_record_hook;
+            #endif
+        }
 
-        #if CAM_EXT_TV_RANGE	
-	if (my_ncmp(name, "tExpDrvTas", 10) == 0){
-	    *entry = (long)exp_drv_task;
-	}
+        #if CAM_EXT_TV_RANGE
+            if (my_ncmp(name, "tExpDrvTas", 10) == 0) {
+               *entry = (long)exp_drv_task;
+            }
         #endif
 
-	core_hook_task_create(pNewTcb);
+        core_hook_task_create(pNewTcb);
     }
 }
 
@@ -140,82 +130,79 @@ void startup()
     long *ptr;
 
     // sanity check
-    if ((long)&link_bss_end > (MEMISOSTART + MEMISOSIZE)){
-	started();
-	shutdown();
+    if ((long)&link_bss_end > (MEMISOSTART + MEMISOSIZE)) {
+        started();
+        shutdown();
     }
 
     // initialize .bss senment
     while (bss<&link_bss_end)
-	*bss++ = 0;
+        *bss++ = 0;
 
     // fill memory with this magic value so we could see what
     // parts of memory were or not used
-#if 0
-    for (ptr=(void*)MEMBASEADDR;((long)ptr)<MEMISOSTART;ptr+=4){
-	ptr[0]=0x55555555;
-	ptr[1]=0x55555555;
-	ptr[2]=0x55555555;
-	ptr[3]=0x55555555;
-    }
-#endif
+    #if 0
+        for (ptr=(void*)MEMBASEADDR;((long)ptr)<MEMISOSTART;ptr+=4) {
+            ptr[0]=0x55555555;
+            ptr[1]=0x55555555;
+            ptr[2]=0x55555555;
+            ptr[3]=0x55555555;
+        }
+    #endif
 
     boot();
 }
 
 #if CAM_CONSOLE_LOG_ENABLED
+    #define DEV_HDR_WRITE_OFFSET (0x14C/4)
 
-#define DEV_HDR_WRITE_OFFSET (0x14C/4)
+    typedef int DEV_HDR;
 
-typedef int DEV_HDR;
+    int (*_tyWriteOrig)(DEV_HDR *hdr, char *buf, int len);
 
-int (*_tyWriteOrig)(DEV_HDR *hdr, char *buf, int len);
-
-
-int hook_tyWriteOrig(DEV_HDR *hdr, char *buf, int len)
-{
-	// Slow, but stable writes
-	FILE *fd = fopen("A/stdout.txt", "a");
-	if (fd) {
-	    fwrite(buf, 1, len, fd);
-	    fclose(fd);
-	}
-
-    return _tyWriteOrig(hdr, buf, len);
-
-}
-
-void console_init()
-{
-    DEV_HDR *DRV_struct;
-
-    DRV_struct = _iosDevFind("/tyCo/0", 0);
-
-    _tyWriteOrig = (void*)DRV_struct[DEV_HDR_WRITE_OFFSET];
-
-	FILE *fd = fopen("A/chdklog.txt", "a");
-	if (fd) {
-	    // can't be used with "Fut" API
-	    //fprintf(fd, "DRV_struct: %x, _tyWriteOrig: %x\n", DRV_struct, _tyWriteOrig);
-	    char buf[256];
-	    int buflen = sprintf(buf, "DRV_struct: %x, _tyWriteOrig: %x\n", DRV_struct, _tyWriteOrig);
-	    fwrite(buf, 1, buflen, fd);
-	}
-
-	FILE *fdout = fopen("A/stdout.txt", "r");
-	if (fdout)
-	{
-        DRV_struct[DEV_HDR_WRITE_OFFSET] = (int)hook_tyWriteOrig;
-        fclose(fdout);
-	    // fprintf(fd, "tyWrite replaced, camera log enabled\n");
-	    fwrite("tyWrite replaced, camera log enabled\n", 1, sizeof("tyWrite replaced, camera log enabled\n"), fd);
+    int hook_tyWriteOrig(DEV_HDR *hdr, char *buf, int len)
+    {
+        // Slow, but stable writes
+        FILE *fd = fopen("A/stdout.txt", "a");
+        if (fd) {
+            fwrite(buf, 1, len, fd);
+            fclose(fd);
+        }
+        return _tyWriteOrig(hdr, buf, len);
     }
 
-	if (fd)
-	{
-	    fclose(fd);
-	}
+    void console_init()
+    {
+        DEV_HDR *DRV_struct;
 
-}
+        DRV_struct = _iosDevFind("/tyCo/0", 0);
+
+        _tyWriteOrig = (void*)DRV_struct[DEV_HDR_WRITE_OFFSET];
+
+        // Create/append chdklog.txt
+        FILE *fd = fopen("A/chdklog.txt", "a");
+        if (fd) {
+            // can't be used with "Fut" API
+            //fprintf(fd, "DRV_struct: %x, _tyWriteOrig: %x\n", DRV_struct, _tyWriteOrig);
+            char buf[256];
+            int buflen = sprintf(buf, "DRV_struct: %x, _tyWriteOrig: %x\n", DRV_struct, _tyWriteOrig);
+            fwrite(buf, 1, buflen, fd);
+        }
+
+        // Write stdout.txt
+        FILE *fdout = fopen("A/stdout.txt", "r");   // Write stdout.txt if file exist
+        //FILE *fdout = fopen("A/stdout.txt", "a");   // Create/append stdout.txt
+        if (fdout) {
+            DRV_struct[DEV_HDR_WRITE_OFFSET] = (int)hook_tyWriteOrig;
+            fclose(fdout);
+            // fprintf(fd, "tyWrite replaced, camera log enabled\n");
+            fwrite("tyWrite replaced, camera log enabled\n", 1, sizeof("tyWrite replaced, camera log enabled\n"), fd);
+        }
+
+        // close filestream fd if exist
+        if (fd) {
+            fclose(fd);
+        }
+    }
 
 #endif
