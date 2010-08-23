65221048d0565482c2c63b7da137c972846e2951
pixeldoc <pixeldoc2000@googlemail.com>
2010-08-23 19:38:14 +0200

    ixus300_sd4000 updated

--- platform/ixus300_sd4000/lib.c
+++ platform/ixus300_sd4000/lib.c
@@ -1,35 +1,34 @@
 #include "platform.h"
 
-void shutdown()
-{
-    volatile long *p = (void*)0xc02200a0;
-        
+void shutdown() {
+    volatile long *p = (void*)0xC02200C4;
+
     asm(
          "MRS     R1, CPSR\n"
          "AND     R0, R1, #0x80\n"
          "ORR     R1, R1, #0x80\n"
          "MSR     CPSR_cf, R1\n"
-         :::"r1","r0");
-        
+         :::"r1","r0"
+    );
+
     *p = 0x44;
 
     while(1);
 }
 
 
-#define LED_PR 0xc02200C4
+#define LED_PR 0xC02200C4    // Green Led (backside)
 
-void debug_led(int state)
-{
+void debug_led(int state) {
     volatile long *p=(void*)LED_PR;
     if (state)
-	p[0]=0x46;
+        p[0]=0x46;
     else
-	p[0]=0x44;
+        p[0]=0x44;
 }
 
 #define LED_AF 0xc0220080
 
-int get_flash_params_count(void){
- return 114;
+int get_flash_params_count(void) {
+    return 114;
 }
