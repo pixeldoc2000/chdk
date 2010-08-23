2d9c7b5d1a8425a68b5e53eec867aa6240a733b0
pixeldoc <pixeldoc2000@googlemail.com>
2010-08-23 19:38:14 +0200

    ixus300_sd4000 updated

--- loader/ixus300_sd4000/resetcode/main.c
+++ loader/ixus300_sd4000/resetcode/main.c
@@ -6,19 +6,19 @@
  * Make sure stack is not used.
  */
 
-//#define DP (void*)0xC02200C4    // direct-print (blue)
-#define DP (void*)0xC0220130    // ToDo: from http://chdk.wikia.com/wiki/SD1400IS
+// DEBUG LED STUFF
+#define DP (void*)0xC0220130    // Greed Led at th Backside
 #define DELAY 5000000
 
 void __attribute__((noreturn)) copy_and_restart(void *dst_void, const void *src_void, long length) {
 
-        // DEBUG: turned off later, so assumed to be power
-        volatile long *p = (void*)DP;
-        int counter;
+        // DEBUG LED STUFF
+        //volatile long *p = (void*)DP;
+        //int counter;
 
         // DEBUG: blink led
-        counter = DELAY; *p = 0x46;  while (counter--) { asm("nop\n nop\n"); };
-        counter = DELAY; *p = 0x44;  while (counter--) { asm("nop\n nop\n"); };
+        //counter = DELAY; *p = 0x46;  while (counter--) { asm("nop\n nop\n"); };
+        //counter = DELAY; *p = 0x44;  while (counter--) { asm("nop\n nop\n"); };
 
         {
                 char *dst = dst_void;
@@ -44,13 +44,13 @@ void __attribute__((noreturn)) copy_and_restart(void *dst_void, const void *src_
         }
 
         // DEBUG: blink again
-        counter = DELAY; *p = 0x46;  while (counter--) { asm("nop\n nop\n"); };
-        counter = DELAY; *p = 0x44;  while (counter--) { asm("nop\n nop\n"); };
+        //counter = DELAY; *p = 0x46;  while (counter--) { asm("nop\n nop\n"); };
+        //counter = DELAY; *p = 0x44;  while (counter--) { asm("nop\n nop\n"); };
 
         // DEBUG: jump to regular firmware-boot (causing a boot loop)
-        dst_void = (void*) 0xFF810000;
+        //dst_void = (void*) 0xFF810000;
 
-        // resetcode here:
+        // ROM:FF83BBFC resetcode here:
         asm volatile (
                         "MRS     R0, CPSR\n"
                         "BIC     R0, R0, #0x3F\n"
@@ -83,22 +83,23 @@ void __attribute__((noreturn)) copy_and_restart(void *dst_void, const void *src_
                         "MCR     p15, 0, R0,c7,c10, 4\n"
                         "MCR     p15, 0, R0,c7,c5\n"
                         "MCR     p15, 0, R0,c7,c6\n"
-                        "MOV     R0, #0x40000006\n"
+                        "MOV     R0, #0x80000006\n"
                         "MCR     p15, 0, R0,c9,c1\n"
                         "MCR     p15, 0, R0,c9,c1, 1\n"
                         "MRC     p15, 0, R0,c1,c0\n"
                         "ORR     R0, R0, #0x50000\n"
                         "MCR     p15, 0, R0,c1,c0\n"
                         "LDR     R0, =0x12345678\n"
-                        "MOV     R1, #0x40000000\n"
+                        "MOV     R1, #0x80000000\n"
                         "STR     R0, [R1,#0xFFC]\n"
                         //"LDR     R0, =0xFF810000\n"   // original jump-vector
-                        "MOV     R0, %0\n"              // new jump-vector
+                        "MOV     R0, %0\n"              // new jump-vector +
                         "LDMFD   SP!, {R4,LR}\n"
-                        //"BX        %0\n"
                         "BX      R0\n"
-                        : : "r"(dst_void) : "memory","r0","r1","r2","r3","r4");
-        //);
+                        : : "r"(dst_void) : "memory","r0","r1","r2","r3","r4"
+        );
 
         while(1);
 }
+
+// NEXT: core/entry.S
