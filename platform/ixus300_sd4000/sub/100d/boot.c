f988b37d2063b7d32f723967743cfe4298aea63f
pixeldoc <pixeldoc2000@googlemail.com>
2010-08-26 07:52:49 +0200

    ixus300_sd4000 updated, does still shutdown after 3 seconds with assert from SpyTask at Mount.c

--- platform/ixus300_sd4000/sub/100d/boot.c
+++ platform/ixus300_sd4000/sub/100d/boot.c
@@ -16,11 +16,11 @@ void boot();
 
 
 void boot() {    //#fs
-    long *canon_data_src = (void*)0xFFC206D4;    // ROM:FF810130 ?!?
+    long *canon_data_src = (void*)0xFFC206D4;    // ROM:FF810130
     long *canon_data_dst = (void*)0x1900;        // ROM:FF810134
-    long canon_data_len = 0xF244 - 0x1900;       // ROM:FF810618 data_end - data_start ?!?
-    long *canon_bss_start = (void*)0xF244;       // just after data ?!?
-    long canon_bss_len = 0x14B394 - 0xF244;       // ROM:FF81014C ?!?
+    long canon_data_len = 0xF244 - 0x1900;       // ROM:FF810138 data_end - data_start
+    long *canon_bss_start = (void*)0xF244;       // ROM:FF810138 just after data
+    long canon_bss_len = 0x14B394 - 0xF244;      // ROM:FF81014C
 
     long i;
 
@@ -56,6 +56,7 @@ void boot() {    //#fs
 
     // jump to init-sequence that follows the data-copy-routine
     asm volatile ("B      sub_FF810354_my\n");
+    //asm volatile ("B      sub_FF810354\n");
 }; //#fe
 
 
@@ -100,7 +101,6 @@ void __attribute__((naked,noinline)) sub_FF810354_my() {    //#fs
 
 void __attribute__((naked,noinline)) sub_FF811198_my() { //#fs
         asm volatile (
-                //"STR     LR, [SP,#var_4]!
                 "STR     LR, [SP,#-4]!\n"       // inspired by original CHDK-code
                 "SUB     SP, SP, #0x74\n"
                 "MOV     R0, SP\n"
@@ -200,7 +200,7 @@ void __attribute__((naked,noinline)) uHwSetup_my() {    //#fs
                 "LDMFD   SP!, {R4,LR}\n"
 
                 // DEBUG LED ---> OK <------------------
-                // "B       sub_FF81FAF0\n"    // taskcreate_Startup() ROM:FF81FAF0
+                //"B       sub_FF81FAF0\n"    // taskcreate_Startup() ROM:FF81FAF0
                 "B       CreateTask_Startup_my\n"    // +
         );
 }; //#fe
@@ -239,12 +239,13 @@ void __attribute__((naked,noinline)) CreateTask_Startup_my() { //#fs
 
                 // DEBUG LED ---> OK <------------------
                 //"ADR     R3, =0xFF81FA8C\n"    // original: task_Startup()
+                //"LDR     R3, =0xFF81FA8C\n"    // + gcc does not like ADR
                 "LDR     R3, =task_Startup_my\n"    // + ROM:FF81FA8C
 
                 "MOV     R2, #0\n"
                 "MOV     R1, #0x19\n"
                 //"ADR     R0, =0xFF81FB7C\n"    // "Startup"
-                "LDR     R0, =0xFF81FB7C\n"    // + "Startup"
+                "LDR     R0, =0xFF81FB7C\n"    // + gcc does not like ADR
                 "BL      sub_FF81E83C\n"       // eventproc_export_CreateTask() KerTask.c:163
                 "MOV     R0, #0\n"
                 "LDMFD   SP!, {R12,PC}\n"
@@ -259,7 +260,7 @@ void __attribute__((naked,noinline)) task_Startup_my() { //#fs
                 "BL      sub_FF81650C\n"    // taskcreate_ClockSave()
                 "BL      sub_FF835674\n"
                 "BL      sub_FF833808\n"
-                //"BL      j_nullsub_217\n"
+                "BL      sub_FF83BD30\n"    // j_nullsub_217\n"
                 "BL      sub_FF83BF1C\n"
 
                 // with StartSdInit() Camera does not show anything and restart
@@ -270,7 +271,7 @@ void __attribute__((naked,noinline)) task_Startup_my() { //#fs
         //debug_led(1);
         //debug_led(0);
 
-        //CreateTask_spytask();    // +
+        CreateTask_spytask();    // +
 
         //debug_led(1);
         //debug_led(0);
@@ -283,6 +284,7 @@ void __attribute__((naked,noinline)) task_Startup_my() { //#fs
                 "BL      sub_FF83C0C4\n"
 
                 "BL      sub_FF834434\n"    // taskcreate_PhySw()
+                //"BL      sub_FF834434_my\n"    // + (from SD990)
         );
 
         //CreateTask_PhySw();    // +
@@ -295,13 +297,16 @@ void __attribute__((naked,noinline)) task_Startup_my() { //#fs
                 "BL      sub_FF8379F8\n"     // ToDo: task_ShootSeqTask Mod here ?
 
                 "BL      sub_FF83C0DC\n"
-                //"BL      nullsub_201\n"
+                "BL      sub_FF8316A8\n"    // nullsub_201\n"
                 "BL      sub_FF833254\n"    // "Battery.c:0\n"
                 "BL      sub_FF83BAC8\n"    // taskcreate_Bye()
                 "BL      sub_FF8337BC\n"
                 "BL      sub_FF8331F0\n"
                 "BL      sub_FF832318\n"
+
                 "BL      sub_FF83CB24\n"
+                //"BL      sub_FF83CB24_my\n"    // + SDHC support
+
                 "BL      sub_FF8331C8\n"
 
                 //"LDMFD   SP!, {R4,LR}\n"
@@ -311,17 +316,80 @@ void __attribute__((naked,noinline)) task_Startup_my() { //#fs
 
         //debug_led(1);
         //debug_led(0);
+        //core_spytask_can_start();
 
         asm volatile (
                 "LDMFD   SP!, {R4,LR}\n"    // +
         );
 }; //#fe
 
+// looks generic
 void CreateTask_spytask() {    //#fs
         _CreateTask("SpyTask", 0x19, 0x2000, core_spytask, 0);
 
 };    //#fe
 
-void CreateTask_PhySw() { //#fs 
+// looks generic
+void CreateTask_PhySw() {    //#fs
         _CreateTask("PhySw", 0x18, 0x800, mykbd_task, 0);
-}; //#fe
+};    //#fe
+
+/*
+void __attribute__((naked,noinline)) sub_FF834434_my() {    //#fs
+ROM:FF834434                 STMFD   SP!, {R3-R5,LR}
+ROM:FF834438                 LDR     R4, =0x1C28
+ROM:FF83443C                 LDR     R0, [R4,#0x10]
+ROM:FF834440                 CMP     R0, #0
+ROM:FF834444                 BNE     loc_FF834468
+ROM:FF834448                 MOV     R3, #0
+ROM:FF83444C                 STR     R3, [SP,#0x10+var_10]
+ROM:FF834450                 ADR     R3, task_PhySw
+ROM:FF834454                 MOV     R2, #0x800
+ROM:FF834458                 MOV     R1, #0x17
+ROM:FF83445C                 ADR     R0, aPhysw      ; "PhySw"
+ROM:FF834460                 BL      KernelCreateTask ; LOCATION: KernelMisc.c:19
+ROM:FF834464                 STR     R0, [R4,#0x10]
+ROM:FF834468
+ROM:FF834468 loc_FF834468                            ; CODE XREF: taskcreate_PhySw+10j
+ROM:FF834468                 BL      sub_FF861F04
+ROM:FF83446C                 BL      sub_FF88DA70
+ROM:FF834470                 BL      sub_FF835CE4
+ROM:FF834474                 CMP     R0, #0
+ROM:FF834478                 LDREQ   R1, =0x32EA0
+ROM:FF83447C                 LDMEQFD SP!, {R3-R5,LR}
+ROM:FF834480                 BEQ     eventproc_export_OpLog.Start
+ROM:FF834484                 LDMFD   SP!, {R3-R5,PC}
+};    //#fe
+*/
+
+/*
+// SDHC Support
+void __attribute__((naked,noinline)) sub_FF83CB24_my() {    //#fs
+                "STMFD   SP!, {R4,LR}\n"
+                "BL      sub_FF8966D8\n"
+                "BL      sub_FF835CE4\n"
+                "CMP     R0, #1\n"
+                "BNE     loc_FF83CB44\n"
+                "BL      sub_FF89973C\n"
+                "LDMFD   SP!, {R4,LR}\n"
+                "B       sub_FF835D84\n"    // StartFactoryModeController()
+            "loc_FF83CB44:\n"
+                "BL      sub_FF8987AC\n"
+                "LDR     R4, =0x1D7C\n"
+                "LDR     R0, [R4,#4]\n"
+                "CMP     R0, #0\n"
+                "LDMNEFD SP!, {R4,PC}\n"
+                "MOV     R1, #0\n"
+
+                //"LDR     R0, =sub_FF83C6E8\n"
+                "LDR     R0, =sub_FF83C6E8_my\n"   // +
+
+                "BL      sub_FF893154\n"    // eventproc_export_CreateController()
+                "STR     R0, [R4,#4]\n"
+                "LDMFD   SP!, {R4,PC}\n"
+};    //#fe
+
+void __attribute__((naked,noinline)) sub_FF83C6E8_my() {    //#fs
+
+};    //#fe
+*/
