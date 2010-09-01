5c801de1dab1ec08fe65d1f000e41c7e5ca20032
pixeldoc <pixeldoc2000@googlemail.com>
2010-09-01 14:59:39 +0200

    ixus300_sd4000 vid_bitmap_refresh() and enabled_refresh_physical_screen() added, now CHDK UI does work (only in Greyscale), Display ration is still not correct

--- include/camera.h
+++ include/camera.h
@@ -2198,6 +2198,10 @@
 #elif defined (CAMERA_ixus300_sd4000)
     #define CAM_PROPSET                 2
     #define CAM_DRYOS                   1
+    #define CAM_DRYOS_2_3_R39           1
+
+    #undef CAM_UNCACHED_BIT             // shut up compiler
+    #define CAM_UNCACHED_BIT            0x40000000
 
     #define CAM_RAW_ROWPIX              3336   // for new 8 MP
     #define CAM_RAW_ROWS                2480   // for new 8 MP
@@ -2207,11 +2211,12 @@
     #define CAM_HAS_ND_FILTER           1
     #undef CAM_HAS_IRIS_DIAPHRAGM
     #undef  CAM_USE_ZOOM_FOR_MF
-    //#define CAM_ADJUSTABLE_ALT_BUTTON   1
     //#define CAM_AF_SCAN_DURING_VIDEO_RECORD 1
     //#define CAM_EV_IN_VIDEO             1
     #undef  CAM_VIDEO_CONTROL
     //#define DNG_SUPPORT                 1
+    //#define CAM_HAS_JOGDIAL             1
+
     // pattern
     #define cam_CFAPattern 0x01000201 // Green  Blue  Red  Green
     // color
@@ -2230,8 +2235,55 @@
     #define CAM_ACTIVE_AREA_Y2 2474
     // camera name
     #define PARAM_CAMERA_NAME 4 // parameter number for GetParameterData
-
     #define DNG_EXT_FROM ".CR2"
+
+    // ---------------------> from SX200IS <------------------------------
+    /*
+    //nandoide sept-2009
+    #undef CAM_USES_ASPECT_CORRECTION
+    #undef CAM_USES_ASPECT_YCORRECTION
+    #define CAM_USES_ASPECT_CORRECTION  1  //camera uses the modified graphics primitives to map screens an viewports to buffers more sized
+    #define CAM_USES_ASPECT_YCORRECTION  0  //only uses mappings on x coordinate
+
+    // reyalp TODO not sure this stuff belongs in camera.h there will probably only be a few different setups
+    // maybe we can just have one CAM_ options that picks what to use ?
+    // values need to be better documented
+    // reyalp - I guess these are bitmap ?
+    //default mappings
+    #undef ASPECT_XCORRECTION
+    #define ASPECT_XCORRECTION(x)  ( ( ((x)<<3) + (x) )  >>2 )   //correction x*screen_buffer_width/screen_width = x*720/320 = x*9/4 = (x<<3 + x)>>2
+
+    //grids
+    #undef ASPECT_GRID_XCORRECTION
+    #define ASPECT_GRID_XCORRECTION(x)  ( ((x)<<3)/9  )  //grids are designed on a 360x240 basis and screen is 320x240, we need x*320/360=x*8/9
+    #undef ASPECT_GRID_YCORRECTION
+    #define ASPECT_GRID_YCORRECTION(y)  ( (y) )       //y correction for grids  made on a 360x240 As the buffer is 720x240 we have no correction here.
+
+    //viewport
+    #undef ASPECT_VIEWPORT_XCORRECTION
+    #define ASPECT_VIEWPORT_XCORRECTION(x) ASPECT_GRID_XCORRECTION(x) //viewport is 360x240 and screen 320x240, we need x*320/360=x*8/9, equal than grids, used by edgeoverlay
+    #undef ASPECT_VIEWPORT_YCORRECTION
+    #define ASPECT_VIEWPORT_YCORRECTION(y) ( (y) )
+    #undef EDGE_HMARGIN
+    #define EDGE_HMARGIN 20
+
+    //games mappings
+   #undef GAMES_SCREEN_WIDTH
+   #undef GAMES_SCREEN_HEIGHT
+   #define GAMES_SCREEN_WIDTH 360
+   #define GAMES_SCREEN_HEIGHT 240
+   #undef ASPECT_GAMES_XCORRECTION
+   // 720/360=2 same aspect than grids and viewport but another approach: there is a lot of corrections to do in game's code, and we decide to paint directly on display buffer wirh another resolution
+   // used by gui.c that configures the draw environment (trhough new draw_gui function) depending on gui_mode: we have then 360x240 for games (but deformed output:circles are not circles) and 320x240 for
+   // other modes in perfect aspect ratio 4/3: slightly better visualization: file menus more readable, ...
+   #define ASPECT_GAMES_XCORRECTION(x)   ( ((x)<<1) )
+   #undef ASPECT_GAMES_YCORRECTION
+   #define ASPECT_GAMES_YCORRECTION(y)   ( (y) )  //none
+
+   //zebra letterbox for saving memory
+   #undef ZEBRA_HMARGIN0
+   #define ZEBRA_HMARGIN0  30 //this 30 rows are not used by the display buffer is 720x240 effective, no 960x270, i.e. (270-240) reduction in widht possible but not done (more difficult to manage it and slower).
+   */
 //----------------------------------------------------------
 
 
