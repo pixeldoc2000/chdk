c5ce27007f27a56c4181ba7a061d852a8a32b752
pixeldoc <pixeldoc2000@googlemail.com>
2011-01-06 21:46:58 +0100

    ixus300_sd4000 comments updated

--- include/camera.h
+++ include/camera.h
@@ -2415,7 +2415,7 @@
 
     //zebra letterbox for saving memory
     #undef ZEBRA_HMARGIN0
-    #define ZEBRA_HMARGIN0  30    //this 30 rows are not used by the display buffer is 720x240 effective, no 960x270, i.e. (270-240) reduction in widht possible but not done (more difficult to manage it and slower).
+    #define ZEBRA_HMARGIN0  30    // this 30 rows are not used by the display buffer is 720x240 effective, no 960x270, i.e. (270-240) reduction in widht possible but not done (more difficult to manage it and slower).
 //----------------------------------------------------------
 
 
--- platform/ixus300_sd4000/main.c
+++ platform/ixus300_sd4000/main.c
@@ -34,22 +34,25 @@ void startup() {
 
 // F/2.0 - 5.3
 // 4.9 - 18.6mm
-static const int fl_tbl[] = {5800, 6420, 7060, 7700, 8340, 9950, 11550, 13160, 14750, 17150, 19570, 22760, 26750, 30750, 34800};    // ToDo
+static const int fl_tbl[] = {5800, 6420, 7060, 7700, 8340, 9950, 11550, 13160, 14750, 17150, 19570, 22760, 26750, 30750, 34800};    // ToDo, convert a focus position to a focal length (* 1000), http://chdk.setepontos.com/index.php?topic=5722.msg59333#msg59333
 #define NUM_FL (sizeof(fl_tbl)/sizeof(fl_tbl[0]))
-#define CF_EFL 60345    // ToDo
+#define CF_EFL 60345    // ToDo, convert a camera focal length to a 35mm effective focal length, http://chdk.setepontos.com/index.php?topic=5722.msg59333#msg59333
 
 const int zoom_points = NUM_FL;
 
+// ToDo, http://chdk.setepontos.com/index.php?topic=5722.msg59333#msg59333
 int get_effective_focal_length(int zp) {
     return (CF_EFL*get_focal_length(zp))/10000;
 }
 
+// ToDo
 int get_focal_length(int zp) {
     if (zp<0) return fl_tbl[0];
     else if (zp>NUM_FL-1) return fl_tbl[NUM_FL-1];
     else return fl_tbl[zp];
 }
 
+// ToDo
 int get_zoom_x(int zp) {
     if (zp<1) return 10;
     else if (zp>NUM_FL-1) return fl_tbl[NUM_FL-1]*10/fl_tbl[0];
