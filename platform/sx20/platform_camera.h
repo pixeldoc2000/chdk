00a7944528068d5bb75850218686b837f78dd8c5
reyalP <reyalP@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2011-04-16 06:09:28 +0000

    sx20 updates from philmoz in http://chdk.setepontos.com/index.php?topic=650.msg64760#msg64760 - enable startup crash fix - exmem support (102d version only)
    git-svn-id: http://tools.assembla.com/svn/chdk/trunk@1146 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- platform/sx20/platform_camera.h
+++ platform/sx20/platform_camera.h
@@ -115,5 +115,9 @@
    #undef EDGE_HMARGIN
    #define EDGE_HMARGIN 28
    #define CAM_CHDK_PTP 1
+
+	#define	CAM_STARTUP_CRASH_FILE_OPEN_FIX	1				// enable fix for camera crash at startup when opening the conf / font files
+															// see http://chdk.setepontos.com/index.php?topic=6179.0
+
 //----------------------------------------------------------
 
--- platform/sx20/sub/102d/boot.c
+++ platform/sx20/sub/102d/boot.c
@@ -176,9 +176,14 @@ void __attribute__((naked,noinline)) sub_FF811178_my() {
                  "BL      sub_FFB36ACC\n" // changed since 102b
                  "MOV     R0, #0x53000\n"
                  "STR     R0, [SP,#4]\n"
-//               "LDR     R0, =0x13DD6C\n" // -  // changed since 102b
-                 "LDR     R0, =new_sa\n"   // +
-                 "LDR     R0, [R0]\n"      // +
+
+#if defined(OPT_CHDK_IN_EXMEM)
+               "LDR     R0, =0x13DD6C\n" // -  // changed since 102b
+#else
+               "LDR     R0, =new_sa\n"   // otherwise use patched value
+               "LDR     R0, [R0]\n"      // 
+#endif
+
                  "LDR     R2, =0x2F9C00\n"
                  "LDR     R1, =0x2F24A8\n"
                  "STR     R0, [SP,#8]\n"
