e95082b48a9a7d5c1c424fc64376dc9e754fd2e8
reyalP <reyalP@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2011-04-25 02:38:48 +0000

    new dancing bits flavor, see http://chdk.setepontos.com/index.php?topic=6241.msg65008#msg65008
    git-svn-id: http://tools.assembla.com/svn/chdk/trunk@1159 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- tools/dancingbits.c
+++ tools/dancingbits.c
@@ -12,13 +12,14 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#define VITALY 5
+#define VITALY 6
 unsigned char _chr_[VITALY][8] = {
 									{ 4,6,1,0,7,2,5,3 }, // original flavor
 									{ 5,3,6,1,2,7,0,4 }, // nacho cheese sx200is, ixus100_sd780, ixu95_sd1200, a1100, d10
 									{ 2,5,0,4,6,1,3,7 }, // mesquite bbq ixus200_sd980, sx20 (dryos r39)
 									{ 4,7,3,2,6,5,0,1 }, // cool ranch a3100 (dryos r43)
 									{ 3,2,7,5,1,4,6,0 }, // cajun chicken s95 (dryos r45)
+									{ 0,4,2,7,3,6,5,1 }, // spicy wasabi sx220 (dryos r47)
 								};
 
 unsigned char dance(unsigned char allbest, int fudgey);
